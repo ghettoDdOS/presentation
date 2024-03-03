a0e16dca4b9f5adbe32f3432d0f5aae530bc676d
ghettoDdOS <pen.egor2002@gmail.com>
2024-03-03 14:35:07 +0300

    first commit

---
+++ components/BankChart.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+import {
+  BarElement,
+  CategoryScale,
+  Chart as ChartJS,
+  Legend,
+  LinearScale,
+  Title,
+  Tooltip,
+} from 'chart.js'
+import { Bar } from 'vue-chartjs'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+const chartData = {
+  labels: [
+    'Райффайзенбанк',
+    'ВТБ',
+    'Тинькофф Банк',
+    'Уральский Банк Реконструкции и Развития',
+    'МТС Банк',
+    'Русский Стандарт',
+    'Альфа-Банк',
+    'Росбанк',
+    'Банк Открытие',
+    'Промсвязьбанк',
+    'Россельхозбанк',
+    'Хоум Кредит Банк',
+    'Газпромбанк',
+    'Совкомбанк',
+    'Ак Барс',
+    'СберБанк',
+    'Почта Банк',
+    'Банк Уралсиб',
+    'Московский Кредитный Банк',
+    'ЮниКредит Банк',
+  ],
+  datasets: [
+    {
+      label: 'Коммуникации',
+      backgroundColor: '#1F2544',
+      data: [
+        24, 26, 18, 21.3, 18, 19.3, 11, 17.3, 11, 15, 30, 18, 11, 9, 14, 16, 11,
+        14.3, 9, 11,
+      ],
+    },
+    {
+      label: 'Платежные сервисы и технологии',
+      backgroundColor: '#474F7A',
+      data: [
+        37.1, 39.1, 39.7, 32.1, 38.3, 36.8, 38.2, 39.5, 39, 39.1, 28.4, 29.5,
+        36.1, 37.4, 37, 39.5, 38.2, 33.3, 38.2, 35.4,
+      ],
+    },
+    {
+      label: 'Оформление продуктов',
+      backgroundColor: '#81689D',
+      data: [
+        20, 15, 20, 20, 15, 15, 15, 20, 20, 15, 10, 20, 20, 20, 20, 15, 10, 15,
+        15, 15, 15,
+      ],
+    },
+  ],
+}
+const chartOptions = {
+  responsive: true,
+}
+</script>
+
+<template>
+  <Bar
+    id="bank-chart"
+    :options="chartOptions"
+    :data="chartData"
+  />
+</template>
---
+++ components/CountChart.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+import {
+  CategoryScale,
+  Chart as ChartJS,
+  Legend,
+  LineElement,
+  LinearScale,
+  PointElement,
+  Title,
+  Tooltip,
+} from 'chart.js'
+import { Line } from 'vue-chartjs'
+
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  LineElement,
+  PointElement,
+  CategoryScale,
+  LinearScale
+)
+
+const chartData = {
+  labels: [
+    '2010',
+    '2011',
+    '2012',
+    '2013',
+    '2014',
+    '2015',
+    '2016',
+    '2017',
+    '2018',
+    '2019',
+  ],
+  datasets: [
+    {
+      label: 'Юридические лица',
+      borderColor: '#1F2544',
+      data: [60.9, 67.9, 74.9, 83.8, 87.6, 90.2, 93.5, 96.4, 97.6, 98.4],
+    },
+    {
+      label: 'Физические лица',
+      borderColor: '#474F7A',
+      data: [8.9, 18.2, 25.4, 29.2, 38.9, 66.2, 85.7, 86.2, 89.8, 93.2],
+    },
+  ],
+}
+const chartOptions = {
+  responsive: true,
+}
+</script>
+
+<template>
+  <Line
+    id="count-chart"
+    :options="chartOptions"
+    :data="chartData"
+  />
+</template>
